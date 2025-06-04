c7bfe63f60187dec82cb5f3bf6b3ae4f17c49a32
250318 <250318@DESKTOP-L34QRGM>
2025-06-04 17:48:31 +0900

    20250604

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,27 @@
+package ch01.sec11;
+
+public class Hello {
+	/**
+	 * @author 박찬호
+	 * @param args
+	 */
+	
+	/*
+	 장제목: 1장 자바 시작하기
+	 작성일: 2025.05.19
+	 */
+
+	/*
+	 * 장제목: 1장 자바 시작하기
+	 * 작성일: 2020.05.19
+	 */
+	
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java1");
+		System.out.println("Hello, Java2");
+
+	}
+
+}
---
+++ src/ch02/sec02/TextBlockExample.java
@@ -0,0 +1,14 @@
+package ch02.sec02;
+
+public class TextBlockExample {
+
+	public static void main(String[] args) {
+		String str1 = "" +
+		"{\n" +
+		"\t\"id\":\"winter\",\n" +
+		"\t\"name\":\"눈송이\"\n" +
+		"}";
+		System.out.println(str1);
+	}
+
+}
