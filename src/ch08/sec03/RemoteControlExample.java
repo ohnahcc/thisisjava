2d57c5382ed469d230d1c53be07150b84d64a01d
ohnahcc <qkrcskgh1015@naver.com>
2025-06-24 17:04:45 +0900

    20250624'

---
+++ src/ch08/sec03/RemoteControlExample.java
@@ -0,0 +1,10 @@
+package ch08.sec03;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		System.out.println("리모컨 최대 볼륨: " + RemoteControl.MAX_VOLUME);
+		System.out.println("리모컨 최소 볼륨: " + RemoteControl.MIN_VOLUME);
+	}
+
+}
