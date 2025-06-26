2f0c930f464aacb8a7dda211ae7085df38cf7e4f
ohnahcc <qkrcskgh1015@naver.com>
2025-06-26 17:16:43 +0900

    20250626

---
+++ src/ch08/sec12/InstanceofExample.java
@@ -0,0 +1,24 @@
+package ch08.sec12;
+
+public class InstanceofExample {
+
+	public static void main(String[] args) {
+		Taxi taxi = new Taxi();
+		Bus bus = new Bus();
+		
+		ride(taxi);
+		System.out.println();
+		ride(bus);
+	}
+	public static void ride(Vehicle vehicle) {
+//		if(vehicle instanceof Bus) {
+//			Bus bus = (Bus) vehicle;
+//			bus.checkFare();
+//		}
+		if(vehicle instanceof Bus bus) {
+			bus.checkFare();
+		}
+		vehicle.run();
+	}
+
+}
