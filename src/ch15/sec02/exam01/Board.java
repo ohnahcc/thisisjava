b599c59edc1e147da05c8c38bb366d6fd66d1e16
ohnahcc <qkrcskgh1015@naver.com>
2025-09-02 15:46:50 +0900

    20250902

---
+++ src/ch15/sec02/exam01/Board.java
@@ -0,0 +1,32 @@
+package ch15.sec02.exam01;
+
+public class Board {
+	private String subject;
+	private String content;
+	private String writer;
+	
+	public Board(String subject, String content, String writer) {
+		this.subject = subject;
+		this.content = content;
+		this.writer = writer;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+}
