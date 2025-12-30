648a7ac30615565ccb05ca558fe6b48e6ec37e2f
ohnahcc <qkrcskgh1015@naver.com>
2025-12-30 17:02:46 +0900

    20251230

---
+++ src/ch19/sec03/exam02/EchoClient.java
@@ -0,0 +1,36 @@
+package ch19.sec03.exam02;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class EchoClient {
+
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost", 50001);
+			
+			System.out.println("[클라이언트] 연결 성공");
+			
+			String sendMessage = "나는 자바가 좋아";
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			dos.writeUTF(sendMessage);
+			dos.flush();
+			System.out.println("[클라이언트] 데이터 보냄: " + sendMessage);
+			
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			String receiveMessage = dis.readUTF();
+			System.out.println("[클라이언트] 데이터 받음: " + receiveMessage);
+			
+			socket.close();
+			System.out.println("[클라이언트] 연결 끊음");
+		} catch (UnknownHostException e) {
+			
+		} catch (IOException e) {
+			
+		}
+	}
+
+}
