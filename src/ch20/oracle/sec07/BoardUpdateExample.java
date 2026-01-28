bef22ce89ce44144a916da96ad9a82eb85b46497
ohnahcc <qkrcskgh1015@naver.com>
2026-01-28 17:40:37 +0900

    20260128

---
+++ src/ch20/oracle/sec07/BoardUpdateExample.java
@@ -0,0 +1,52 @@
+package ch20.oracle.sec07;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BoardUpdateExample {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521/orcl",
+					"java",
+					"oracle"
+					);
+			
+			String sql = new StringBuilder()
+					.append("UPDATE boards SET ")
+					.append("btitle=?, ")
+					.append("bcontent=?, ")
+					.append("bfilename=?, ")
+					.append("bfiledata=? ")
+					.append("WHERE bno=?")
+					.toString();
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "눈사람");
+			pstmt.setString(2, "눈으로 만든 사람");
+			pstmt.setString(3, "snowman.jpg");
+			pstmt.setBlob(4, BoardUpdateExample.class.getResourceAsStream("snowman.jpg"));
+			pstmt.setInt(5, 1);
+			int rows = pstmt.executeUpdate();
+			System.out.println("수정된 행 수: " + rows);
+			
+			pstmt.close();
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(conn != null) {
+				try {
+					conn.close();
+					System.out.println("연결 끊기");
+				} catch (SQLException e) {}
+			}
+		}
+	}
+}
---
+++ src/ch20/oracle/sec08/BoardDeleteExample.java
@@ -0,0 +1,43 @@
+package ch20.oracle.sec08;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BoardDeleteExample {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521/orcl",
+					"java",
+					"oracle"
+					);
+			
+			String sql = "DELETE FROM boards WHERE bwriter=?";
+		
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "winter");
+			
+			int rows = pstmt.executeUpdate();
+			System.out.println("삭제된 행 수: " + rows);
+			
+			pstmt.close();
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(conn != null) {
+				try {
+					conn.close();
+					System.out.println("연결 끊기");
+				} catch (SQLException e) {}
+			}
+		}
+	}
+}
